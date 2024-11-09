1e7b158d9839bbabb7d603b31f9f93ad8da2ce35
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2024-11-09 16:12:25 +0100

    Use openapi-fetch on the frontend

--- app/src/pages/Index.svelte
+++ app/src/pages/Index.svelte
@@ -1,6 +1,10 @@
-<script>
+<script lang="ts">
     import Layout from "../Layout.svelte";
     import Navbar from "../lib/components/Navbar.svelte";
+    import client from "../lib/api/client";
+
+    const fetchRecipes = async () => await client.GET("/recipes")
+    let recipeResult = fetchRecipes()
 </script>
 
 <Layout header={Navbar}>
@@ -8,5 +12,22 @@
         <h1>Index</h1>
         <p>Hello, World!</p>
         <a href="/about">About</a>
+        {#await recipeResult}
+            <p>
+                Loading recipes...
+            </p>
+        {:then recipes}
+            {#if !recipes.data || recipes.data.length <= 0}
+                <p>No recipes found</p>
+            {:else}
+                <ul>
+                    {#each recipes.data as recipe}
+                        <li>{recipe.name}</li>
+                    {/each}
+                </ul>
+            {/if}
+        {:catch error}
+            <p>Failed to load recipes</p>
+        {/await}
     </main>
 </Layout>
--- service/errors.go
+++ service/errors.go
@@ -19,7 +19,7 @@ func (e RecipeServiceError) Error() string {
 
 var (
 	ErrUnhandled      = RecipeServiceError{HttpStatusCode: 500, ErrorCode: 1, ErrorMessage: "internal server error"}
-	ErrRecipeNotFound = RecipeServiceError{HttpStatusCode: 404, ErrorCode: 2, ErrorMessage: "no recipe found"}
+	ErrRecipeNotFound = RecipeServiceError{HttpStatusCode: 404, ErrorCode: 2, ErrorMessage: "recipe not found"}
 )
 
 func (p *RecipesService) NewError(ctx context.Context, err error) (r *api.ErrorStatusCode) {
