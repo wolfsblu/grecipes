b6af2503ee049ff4e79aa54813564750b6abb9d2
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2024-11-16 14:13:01 +0100

    Use custom input element

--- app/src/lib/components/recipes/Form.svelte
+++ app/src/lib/components/recipes/Form.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
     import Input from "../../forms/Input.svelte";
+
+    interface Recipe {
+        name: string
+        servings: number | null
+        time: number | null
+    }
+
+    const recipe: Recipe = $state({
+        name: 'wda',
+        servings: null,
+        time: null,
+    })
+
+    const saveRecipe = (e: SubmitEvent) => {
+        e.preventDefault()
+        console.log(recipe)
+    }
 </script>
 
-<form>
-    <Input type="text" label="Name" name="name" />
+<form onsubmit={saveRecipe}>
+    <Input type="text" label="Name" name="name" bind:value={recipe.name}/>
+    <Input type="number" label="Servings" name="servings" bind:value={recipe.servings}/>
+    <Input type="number" label="Time" name="minutes" bind:value={recipe.time}/>
+    <button type="submit">
+        Create
+    </button>
 </form>
+
+<pre>
+{recipe.name}
+</pre>
--- app/src/lib/forms/Input.svelte
+++ app/src/lib/forms/Input.svelte
@@ -1,16 +1,20 @@
-<script lang="ts">
+<script lang="ts" generics="T">
     import type {HTMLInputTypeAttribute} from "svelte/elements";
 
     interface Props {
         label?: string
         name: string
         type: HTMLInputTypeAttribute | null | undefined
+        value: T
     }
 
-    let { label, type }: Props = $props()
+    let {label, value = $bindable(), ...rest}: Props = $props()
+    let fields = {...rest}
 </script>
 
 <label>
     {label}
-    <input class="appearance-none border border-gray-300 outline-none p-2 rounded-sm w-full" {type}>
+    <input class="appearance-none border border-gray-300 outline-none p-2 rounded-sm w-full"
+           {...fields}
+           bind:value={value}>
 </label>
