bc38f0accc1b864cc294608fb7b31ce45d03117c
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2024-11-16 18:25:51 +0100

    Improve fatal error messages

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/swaggest/swgui/v5emb"
 	"github.com/wolfsblu/grecipes/api"
 	"github.com/wolfsblu/grecipes/db"
@@ -12,17 +13,22 @@ import (
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("failed to load .env file", err)
+	}
+
 	dbPath := getEnv("DB_PATH", "tmp/db.sqlite")
 	query, err := db.Connect(dbPath)
 	if err != nil {
-		log.Fatalln("could not connect to database", err)
+		log.Fatalln("failed to connect to the database", err)
 	}
 	svc := &service.RecipesService{
 		Db: query,
 	}
 	handler, err := api.NewServer(svc)
 	if err != nil {
-		log.Fatalln("failed to start API server:", err)
+		log.Fatalln("failed to start api server:", err)
 	}
 
 	mux := http.NewServeMux()
@@ -33,7 +39,7 @@ func main() {
 	host := getEnv("HOST", "127.0.0.1:8080")
 	err = http.ListenAndServe(host, mux)
 	if err != nil {
-		log.Fatalln("failed to start server:", err)
+		log.Fatalln("failed to start web server:", err)
 	}
 }
 
