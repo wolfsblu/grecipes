be284f1cb476f43446ad6c1f297de461f2d3745c
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2024-11-23 09:18:34 +0100

    Move docs to api folder

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
 package main
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target api --clean docs/openapi.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target api --clean api/openapi.yml
 //go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@latest generate
--- routes/docs.go
+++ routes/docs.go
@@ -1,10 +1,10 @@
 package routes
 
 import (
-	"github.com/wolfsblu/go-chef/docs"
+	"github.com/wolfsblu/go-chef/api"
 	"net/http"
 )
 
 func apiDocs(w http.ResponseWriter, r *http.Request) {
-	http.ServeFileFS(w, r, docs.APIDocsFS, "openapi.yml")
+	http.ServeFileFS(w, r, api.APIDocsFS, "openapi.yml")
 }
