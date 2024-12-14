25f89c9d71dd66cdd05ea2ef8af8fb61c6cd67e4
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2024-12-14 15:05:05 +0100

    Fix button markup

--- app/src/lib/components/forms/Button.svelte
+++ app/src/lib/components/forms/Button.svelte
@@ -12,30 +12,28 @@
     let fields = {...rest}
 </script>
 
-<label>
-    <button class="
-    active:bg-orange-100
-    active:shadow-inner
-    active:text-orange-700
-    bg-orange-100
-    flex
-    font-semibold
-    gap-2
-    hover:bg-orange-200
-    hover:text-orange-800
-    items-center
-    px-4
-    py-2
-    rounded
-    text-orange-700
-    {cls}
-    "
-    {...fields}>
-        {#if Icon}
-            <span class="h-4 w-4 relative">
-                <Icon />
-            </span>
-        {/if}
-        {@render children()}
-    </button>
-</label>
+<button class="
+active:bg-orange-100
+active:shadow-inner
+active:text-orange-700
+bg-orange-100
+flex
+font-semibold
+gap-2
+hover:bg-orange-200
+hover:text-orange-800
+items-center
+px-4
+py-2
+rounded
+text-orange-700
+{cls}
+"
+{...fields}>
+    {#if Icon}
+        <span class="h-4 w-4 relative">
+            <Icon />
+        </span>
+    {/if}
+    {@render children()}
+</button>
